2e85d6f5b6f05b4e1d19bac0d7eae0c7f316c957
akaashanky <akaashanky@gmail.com>
2014-04-24 03:48:35 +0530

    Added form validation for url form

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,4 +5,20 @@ angular.module('tldrApp')
     $http.get('/api/awesomeThings').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
     });
+
+    $scope.url="";
+    $scope.submitted=false;
+
+    $scope.submitForm = function(isValid) {
+    		$scope.submitted=true;
+			// check to make sure the form is completely valid
+			if (isValid) {
+				alert('our form is amazing');
+			}
+
+		};
+
+
+
+
   });
