f3f406ff822d74dcc4e80685bb2c073ba8226d70
akaashanky <akaashanky@gmail.com>
2014-04-24 04:36:28 +0530

    Basic UI/backend interaction in place

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,12 +8,25 @@ angular.module('tldrApp')
 
     $scope.url="";
     $scope.submitted=false;
+    $scope.title="Lorem Ipsum";
+    $scope.summary="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, deleniti, quis, hic autem eius saepe dolore eum accusantium nostrum pariatur labore officiis aliquam quas deserunt quo consequatur itaque ea animi!";
 
     $scope.submitForm = function(isValid) {
     		$scope.submitted=true;
 			// check to make sure the form is completely valid
 			if (isValid) {
-				alert('our form is amazing');
+				var formData={};
+				formData.url=$scope.url;
+
+				$http.post('/api/summary', formData)
+					.success(function(data) {
+						$scope.title=data.title;
+						$scope.summary=data.summary;
+					})
+					.error(function(data){
+						$scope.title="Whoops!"
+						$scope.summary="Looks like something went wrong when summarizing the contents of the given URL. The summarizer is designed to work with URLs of articles/blog posts, but it's also possible that it was unable to glean the right contents from the page. Sorry!"
+					});
 			}
 
 		};
